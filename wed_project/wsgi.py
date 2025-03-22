6388cf1af355080196c8814c7d95a5132fdc416b
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-03-22 01:31:37 +0000

    Update wsgi.py

--- wed_project/wsgi.py
+++ wed_project/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wed_project.settings')
 
+# Get the WSGI application
 application = get_wsgi_application()
+
+# Expose the application for Vercel
+app = application
